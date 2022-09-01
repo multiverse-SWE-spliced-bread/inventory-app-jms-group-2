d8b65da0daf200579d3559c8ac1f9a0bf57f7632
shakil-a <ahmed1shakil@gmail.com>
2022-09-01 14:53:43 +0100

    react changes

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -5,7 +5,6 @@ function App() {
 
   return (
     <div>
-     Hello, world!
      <Items />
      
     </div>
--- client/client/src/components/fullItems.js
+++ client/client/src/components/fullItems.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState} from 'react';
 
 export const Items = () => {
     const [allItems, setAllItems] = useState([])
+    const [Item, setItem] = useState([])
     async function itemsList () {
         const res = await fetch('http://localhost:3000/items')
         console.log(res)
@@ -10,6 +11,13 @@ export const Items = () => {
         console.log(data)
         setAllItems(data)
         console.log(allItems)
+    }
+
+    async function oneItem (id) {
+      const res = await fetch(`http://localhost:3000/items/${id}`)
+      const data = await res.json()
+      setItem(data)
+
     }
 
     useEffect(() => {
@@ -21,7 +29,7 @@ export const Items = () => {
   return (
     <div>
         Hello World!
-        {allItems.map(items => <div>{items.title}</div>)}
+        {allItems.map(items => <div onClick={() => oneItem(items.id)}>{items.title}</div>)}
     </div>
     )
   }
