0c79252b7958e938edc17369946b0b5ef0169616
matte-134 <matthew.eland@nandos.co.uk>
2022-09-01 11:19:20 +0100

    cors error

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react';
-// import { Items } from './fullItems';
+import { Items } from './components/fullItems';
 
 function App() {
 
-  const [items, setItems] = useState([])
-  const [allItems, setAllItems] = useState([])
-  async function getItems() {
-    const res = await fetch("http://localhost:3000/items")
-    const data = await res.json()
-    setAllItems(data)
-  }
+  // const [items, setItems] = useState([])
+  // const [allItems, setAllItems] = useState([])
+  // async function getItems() {
+  //   const res = await fetch("http://localhost:3000/items")
+  //   const data = await res.json()
+  //   setAllItems(data)
+  // }
 
-  useEffect(() => {
-    getItems();
-  }, [])
+  // useEffect(() => {
+  //   getItems();
+  // }, [])
 
   return (
     <div>
      Hello, world!
-     
+     {/* <Items /> */}
     </div>
   );
 }
--- client/client/src/components/fullItems.js
+++ client/client/src/components/fullItems.js
@@ -1,6 +1,18 @@
-import {react} from 'react'
+import React, { useState} from 'react';
 
-// export const Items = () => {
 
-    
-// }
+export const Items = async () => {
+    const [allItems, setAllItems] = useState([])
+
+    const res = await fetch('http://localhost:3000/item')
+    console.log(res)
+    const data = await res.json()
+    console.log(data)
+    setAllItems(data)
+  
+  return (
+    <div>
+        {allItems.map(items => <div>{items.title}</div>)}
+    </div>
+    )
+}
