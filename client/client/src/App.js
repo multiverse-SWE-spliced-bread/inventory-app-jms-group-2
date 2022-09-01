d5b3f155e54e19d10a6095e68ce3ffe42ca79fba
matte-134 <matthew.eland@nandos.co.uk>
2022-09-01 11:33:18 +0100

    cors fixed

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <div>
      Hello, world!
-     {/* <Items /> */}
+     <Items />
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 const seed = require('./seedData/seed')
 const { itemsRouter } = require('./routes')
 const port = 3000
@@ -11,6 +13,7 @@ app.use('/items', itemsRouter)
 app.get('/', (req, res) => {
     res.send('Hello!')
 })
+
 
 app.listen(port, () =>  {
     console.log(`express is running on port: ${port}`)
